381cc3402b609c09c4572bb752333b3c80797a63
Gary Janes <gary_janes@hotmail.com>
2018-03-13 17:19:11 +0000

    Fix missing @Service annotation on category service

--- src/main/java/guru/springfamework/services/CategoryServiceImpl.java
+++ src/main/java/guru/springfamework/services/CategoryServiceImpl.java
@@ -4,10 +4,12 @@ import guru.springfamework.api.v1.mapper.CategoryMapper;
 import guru.springfamework.api.v1.model.CategoryDTO;
 import guru.springfamework.repositories.CategoryRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Service
 @AllArgsConstructor
 public class CategoryServiceImpl implements CategoryService {
 
